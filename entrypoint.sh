da7dcaac7f323c0fb5e019902c9036245a568523
Jake Low <hello@jakelow.com>
2019-01-18 17:52:42 -0800

    Make crond log to stderr instead of file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # add backup script to our crontab; the default schedule is daily at midnight
 echo "${CRON_SCHEDULE:-0 0 * * *} /backup.sh" | crontab -
 
-# run cron in foreground
-exec crond -f
+# run cron in foreground and log to STDERR
+exec crond -f -d 8
